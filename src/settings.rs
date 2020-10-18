5ab18c5d39187701f2d3c83c656e3ec87498c1f0
Kostas Kolivas <kwstaskolivas@gmail.com>
2020-10-18 19:07:29 +0300

    Add more error handling in settings module

--- src/settings.rs
+++ src/settings.rs
@@ -1,57 +1,78 @@
 extern crate yaml_rust;
 use std::error::Error;
+use std::fmt;
 
 use yaml_rust::YamlLoader;
-
-pub trait Parseable {
-    fn from_yaml(filename: &'static str) -> Result<Self, Box<dyn Error>> where Self: Sized;
-    fn default() -> Self;
-}
 
 const DEFAULT_NUM_PROCESSORS: usize = 2;
 const DEFAULT_NUM_FEEDERS: usize = 4;
 
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub struct Settings {
-    pub num_processors: usize,
-    pub num_feeders: usize
+    num_processors: usize,
+    num_feeders: usize
 }
 
-impl Parseable for Settings {
-    fn default() -> Settings {
-        Settings {
-            num_processors: DEFAULT_NUM_PROCESSORS,
-            num_feeders: DEFAULT_NUM_FEEDERS
-        }
-    }
-
-    fn from_yaml(filename: &str) -> Result<Settings, Box<dyn Error>> {
-        let contents = std::fs::read_to_string(filename);
+impl Settings {
+    pub fn from_file(filename: &str) -> Result<Settings, Box<dyn Error>> {
+        let contents  = std::fs::read_to_string(filename);
         
         // Return the default settings if the file can't be read
         if contents.is_err() {
             return Ok(Settings::default());
         }
 
-        let docs = YamlLoader::load_from_str(&contents.unwrap())?;
+        Settings::from_string(&contents.unwrap())
+    }
 
-        // Same thing if the file has no content
-        if docs.len() == 0 {
+    pub fn from_string(yml: &str) -> Result<Settings, Box<dyn Error>> {
+        let docs = YamlLoader::load_from_str(&yml)?;
+
+        // Return the default settings if the file is empty
+        if docs.is_empty() {
             return Ok(Settings::default());
         }
 
-        let doc = &docs[0];
-        let workers = &doc["workers"];
+        let workers = &docs[0]["workers"];
         let num_processors = match workers["processors"].as_i64() {
-            Some(processors) => processors as usize,
-            None => DEFAULT_NUM_PROCESSORS
+            Some(processors) => processors as i32,
+            None => DEFAULT_NUM_PROCESSORS as i32
         };
 
         let num_feeders = match workers["feeders"].as_i64() {
-            Some(feeders) => feeders as usize,
-            None => DEFAULT_NUM_FEEDERS
+            Some(feeders) => feeders as i32,
+            None => DEFAULT_NUM_FEEDERS as i32
         };
 
-        Ok(Settings { num_processors, num_feeders })
+        if num_processors <= 0 || num_feeders <= 0 {
+            return Err(Box::new(NonPositiveWorkers));
+        }
+
+        Ok(Settings { num_processors: num_processors as usize, num_feeders: num_feeders as usize })
     }
+
+    pub fn num_processors(&self) -> usize {
+        self.num_processors
+    }
+
+    pub fn num_feeders(&self) -> usize {
+        self.num_feeders
+    }
+    
+    fn default() -> Settings {
+        Settings {
+            num_processors: DEFAULT_NUM_PROCESSORS,
+            num_feeders: DEFAULT_NUM_FEEDERS
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct NonPositiveWorkers;
+impl fmt::Display for NonPositiveWorkers {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Number of workers must be positive")
+    }
+}
+impl Error for NonPositiveWorkers {}
 }

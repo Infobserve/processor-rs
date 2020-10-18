feea600b35d4be1ddb48a69d77bb3edb2177fa11
Kostas Kolivas <kwstaskolivas@gmail.com>
2020-10-18 19:10:58 +0300

    Add sample settings usage in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod settings;
+use settings::Settings;
+
 fn main() {
-    println!("Is this the best processor or what?");
+    let s = Settings::from_file("config.yaml").unwrap();
+
+    println!("Read settings:\nNumber of processors: {} & Number of feeders: {}", s.num_processors(), s.num_feeders());
 }

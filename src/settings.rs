3c5c57eff25412f7148b8330db75b2a6e90a8a3c
Kostas Kolivas <kkolivas@skroutz.gr>
2020-12-28 17:32:27 +0200

    Implement the `Default` trait for Settings struct
    We had already implemented a `Settings::default()` method but recently
    found out that there is a `Default` trait which does pretty much the
    same thing but auto-resolves to the correct type. This commit removes
    the `Settings::default()` method and moves the logic to `Default`'s
    implementation for the `Settings` struct.
    
    Additionally, makes the settings parsing code a bit easier to read by
    removing some `match`es, replacing them with `unwrap_or` for the default
    value.
    
    Lastly, paves the way for a possible change that will allow worker
    numbers to be non-positive

--- src/settings.rs
+++ src/settings.rs
@@ -1,45 +1,60 @@
-use std::error;
+use log::{info, warn};
 use std::fs;
+
+use anyhow::Result;
 
 use yaml_rust::YamlLoader;
 use crate::errors;
 
-const DEFAULT_NUM_PROCESSORS: usize = 1;
-const DEFAULT_NUM_FEEDERS: usize = 1;
+const DEFAULT_NUM_PROCESSORS: i32 = 1;
+const DEFAULT_NUM_FEEDERS: i32 = 1;
 const DEFAULT_YARA_RULE_DIR: &str = "yara-rules/";
 
 #[derive(PartialEq, Debug)]
 pub struct Settings {
     yara_rule_dir: String,
-    num_processors: usize,
-    num_feeders: usize
+    num_processors: i32,
+    num_feeders: i32
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            yara_rule_dir: String::from(DEFAULT_YARA_RULE_DIR),
+            num_processors: DEFAULT_NUM_PROCESSORS,
+            num_feeders: DEFAULT_NUM_FEEDERS
+        }
+    }
 }
 
 impl Settings {
-    /// Loads configuration from a file.
-    /// If the file cannot be read (it doesn't exist or the current user cannot read it),
-    /// the default settings are instead returned
+    /// Loads configuration from a YAML file.
+    /// If the file cannot be read, the default settings are returned instead
     ///
     /// # Arguments
     ///
     /// * `filename`: The fully qualified path to the file to read from
-    pub fn from_file(filename: &str) -> Result<Settings, Box<dyn error::Error>> {
+    ///
+    /// # Returns
+    /// anyhow::Result<Settings>: Will only be Err if the number of any worker (feeder, processor
+    /// or loader) is negative
+    pub fn from_file(filename: &str) -> Result<Self> {
         match fs::read_to_string(filename) {
             Ok(contents) => Settings::from_string(&contents),
-            Err(_) => {
-                log::info!("Could not read configuration file ({}). Loading defaults", filename);
-                Ok(Settings::default())
+            Err(e) => {
+                info!("Could not read configuration file {} ({}). Loading defaults", filename, e);
+                Ok(Default::default())
             }
         }
     }
 
     #[allow(dead_code)]
-    pub fn num_processors(&self) -> usize {
+    pub fn num_processors(&self) -> i32 {
         self.num_processors
     }
 
     #[allow(dead_code)]
-    pub fn num_feeders(&self) -> usize {
+    pub fn num_feeders(&self) -> i32 {
         self.num_feeders
     }
 
@@ -48,12 +63,13 @@ impl Settings {
         &self.yara_rule_dir
     }
 
-    fn from_string(yml: &str) -> Result<Settings, Box<dyn error::Error>> {
+    fn from_string(yml: &str) -> Result<Settings> {
         let docs = YamlLoader::load_from_str(&yml)?;
 
         // Return the default settings if the file is empty
         if docs.is_empty() {
-            return Ok(Settings::default());
+            warn!("Found empty configuration file. Loading default settings");
+            return Ok(Default::default());
         }
 
         let doc = &docs[0];
@@ -65,38 +81,24 @@ impl Settings {
         Settings::build_from_config(rule_dir, processors, feeders)
     }
 
-    fn build_from_config(rule_dir: Option<&str>, processors: Option<i64>,
-                         feeders: Option<i64>) -> Result<Settings, Box<dyn error::Error>> {
-        let yara_rule_dir = match rule_dir {
-            Some(y) => y,
-            None => DEFAULT_YARA_RULE_DIR
-        };
-        let num_processors = match processors {
-            Some(p) => p as i32,
-            None => DEFAULT_NUM_PROCESSORS as i32
-        };
-        let num_feeders = match feeders {
-            Some(p) => p as i32,
-            None => DEFAULT_NUM_FEEDERS as i32
-        };
+    fn build_from_config(
+        rule_dir: Option<&str>,
+        processors: Option<i64>,
+        feeders: Option<i64>
+    ) -> Result<Settings> {
+        let yara_rule_dir = rule_dir.unwrap_or(DEFAULT_YARA_RULE_DIR);
+        let num_processors = processors.unwrap_or(DEFAULT_NUM_PROCESSORS as i64) as i32;
+        let num_feeders = feeders.unwrap_or(DEFAULT_NUM_FEEDERS as i64) as i32;
 
         if num_processors <= 0 || num_feeders <= 0 {
-            return Err(Box::new(errors::NonPositiveWorkersError));
+            return Err(errors::NonPositiveWorkersError.into());
         }
 
         Ok(Settings {
             yara_rule_dir: String::from(yara_rule_dir),
-            num_processors: num_processors as usize,
-            num_feeders: num_feeders as usize 
+            num_processors: num_processors,
+            num_feeders: num_feeders
         })
-    }
-    
-    fn default() -> Settings {
-        Settings {
-            yara_rule_dir: String::from(DEFAULT_YARA_RULE_DIR),
-            num_processors: DEFAULT_NUM_PROCESSORS,
-            num_feeders: DEFAULT_NUM_FEEDERS
-        }
     }
 }
 
@@ -106,13 +108,13 @@ mod tests {
 
     #[test]
     fn it_returns_the_default_for_missing_file() {
-        assert_eq!(Settings::from_file("non-existent.yml").unwrap(), Settings::default());
+        assert_eq!(Settings::from_file("non-existent.yml").unwrap(), Default::default());
     }
 
     #[test]
     fn it_returns_the_default_for_empty_cfg() {
         let yml = "";
-        assert_eq!(Settings::from_string(yml).unwrap(), Settings::default());
+        assert_eq!(Settings::from_string(yml).unwrap(), Default::default());
     }
 
     #[test]
@@ -124,7 +126,11 @@ mod tests {
         "#;
         assert_eq!(
             Settings::from_string(yml).unwrap(),
-            Settings { yara_rule_dir: String::from("foo"), num_processors: 2, num_feeders: DEFAULT_NUM_FEEDERS }
+            Settings {
+                yara_rule_dir: String::from("foo"),
+                num_processors: 2,
+                num_feeders: DEFAULT_NUM_FEEDERS
+            }
         );
     }
 
@@ -136,7 +142,11 @@ mod tests {
         "#;
         assert_eq!(
             Settings::from_string(yml).unwrap(),
-            Settings { yara_rule_dir: String::from(DEFAULT_YARA_RULE_DIR), num_processors: DEFAULT_NUM_PROCESSORS, num_feeders: 5 }
+            Settings {
+                yara_rule_dir: String::from(DEFAULT_YARA_RULE_DIR),
+                num_processors: DEFAULT_NUM_PROCESSORS,
+                num_feeders: 5
+            }
         )
     }
 

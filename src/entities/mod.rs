4c07cc2c4bdfb33cd9adb6230cddfb126e2c3be0
Kostas Kolivas <kwstaskolivas@gmail.com>
2023-09-20 20:34:38 +0300

    Move ProcessedEvent under event.rs
    ProcessedEvent is a very simple struct that only holds a vector of
    FlatMatch objects and provides no implementation. At the same time,
    conceptually, a processed event is an event itself. Due to both of the
    above, moving it under `entities/events.rs` makes sense

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -2,12 +2,10 @@ mod event;
 mod rule_match;
 mod ascii_match;
 mod index_cache;
-mod processed_event;
 mod flat_match;
 
-pub use event::Event;
+pub use event::{Event, ProcessedEvent};
 pub use rule_match::RuleMatch;
 pub use ascii_match::AsciiMatch;
 pub use index_cache::IndexCache;
-pub use processed_event::ProcessedEvent;
 pub use flat_match::FlatMatch;
--- src/entities/processed_event.rs
+++
@@ -1,4 +0,0 @@
-use crate::entities::{FlatMatch, Event};
-
-#[derive(Debug)]
-pub struct ProcessedEvent(pub Event, pub Vec<FlatMatch>);
